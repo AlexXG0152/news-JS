ad3a638801dd2a53e5604bddf76487e1972abd57
AlexXG0152 <34213814+AlexXG0152@users.noreply.github.com>
2021-12-07 00:31:29 +0300

    fix: all stages complete

--- src/components/controller/appLoader.ts
+++ src/components/controller/appLoader.ts
@@ -3,7 +3,7 @@ import Loader from './loader';
 class AppLoader extends Loader {
     apiKey: string;
     constructor() {
-        super('https://newsapi.org/v2/', {
+        super('https://nodenews.herokuapp.com/', {
             apiKey: '0e6720f9c18542a7b8226621498f81c5', // получите свой ключ https://newsapi.org/ 15b437ecd895412e884086ce49317cdd
         });
     }
